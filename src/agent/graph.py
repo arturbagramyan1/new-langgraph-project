02b3af0240b867fdd07ecee8edabf36a617ad4c5
Artur Bagramyan <arthurbaghramian2003@gmail.com>
2025-09-21 22:44:53 +0400

    Update Python version requirement and dependencies in pyproject.toml; refactor graph.py to use updated TavilySearch tool for financial assistance.

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,18 +1,15 @@
 from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
-from langchain_community.tools import TavilySearchResults
+from langchain_tavily import TavilySearch
 
 
-tools = [TavilySearchResults(max_results=10)]
+tools = [TavilySearch(max_results=10)]
 
 # initialize our model and tools (API key is taken from environment)
 llm = ChatOpenAI(model="gpt-4o")
 prompt = """
-    Youre a financial assistant. You have access to the following tools:
-    - TavilySearchResults
-    - You can use the following tools to get information about the stock market:
-    - TavilySearchResults
-    provide financial information for the user.
+    You are a financial assistant. You have access to a web search tool (TavilySearch).
+    Use it to gather up-to-date information before answering.
 """
 
 # Compile the builder into an executable graph
