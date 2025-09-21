c0afd43054d1a067344ec9c9bb26ebc391d5beb3
Artur Bagramyan <arthurbaghramian2003@gmail.com>
2025-09-21 22:29:40 +0400

    Add initial project structure with main.py, set Python version to 3.12, and update pyproject.toml for new-langgraph-project-1. Remove requirements.txt as part of the transition.

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,13 +1,21 @@
 from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
+from langchain_community.tools import TavilySearchResults
+import dotenv
+
+dotenv.load_dotenv()
+
+tools = [TavilySearchResults(max_results=10)]
 
 # initialize our model and tools (API key is taken from environment)
 llm = ChatOpenAI(model="gpt-4o")
 prompt = """
-    You are a helpful AI assistant trained in creating engaging social media content!
-    you have access to two tools: basic_research and get_todays_date. Please get_todays_date then 
-    perform any research if needed, before generating a social media post.
+    Youre a financial assistant. You have access to the following tools:
+    - TavilySearchResults
+    - You can use the following tools to get information about the stock market:
+    - TavilySearchResults
+    provide financial information for the user.
 """
 
 # Compile the builder into an executable graph
-graph = create_react_agent(model=llm, prompt=prompt, name="react_agent", tools=[])
+graph = create_react_agent(model=llm, prompt=prompt, name="react_agent", tools=tools)
